ac8d1e7905ddd7993301f9b0e6900dbc03f0e89b
emanfatima <85811902+emaanfatymah@users.noreply.github.com>
2024-01-31 00:08:26 +0500

    add change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/engineer.jpg";
+import image from "../images/cup.jpg";
 
 const imageAltText = "Eman Fatima - Web Developer. Software Engineer.jpg ";
 
